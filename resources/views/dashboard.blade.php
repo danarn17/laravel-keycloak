cceb54b54fa216b10afb443e87ac0f1b8b4b7ff2
Danar <excaldev@gmail.com>
2024-07-08 21:43:52 +0700

    make dashboard and add login with sso

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,6 +12,57 @@
                     {{ __("You're logged in!") }}
                 </div>
             </div>
-        </div>
+            {{-- <div class="overflow-hidden overflow-x-auto mt-3 p-6 bg-white dark:bg-gray-800 border-b border-gray-200"> --}}
+
+                <div class="relative overflow-auto">
+                    <table class=" text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" class="px-6 py-3">
+                                    NAME
+                                </th>
+                                <th scope="col" class="px-6 py-3" style="max-width: 300px">
+                                    DATA
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    EXPIRED
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    ACCESS_TOKEN
+                                </th>
+                                <td class="px-6 py-4">
+                                    <p class="overflow-hidden break-all whitespace-normal">
+                                        {{ Auth::user()->kc_access_token }}
+                                    </p>
+                                </td>
+                                <td class="px-6 py-4">
+                                    {{ Auth::user()->kc_access_token_expiration }}
+                                </td>
+                            </tr>
+                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    REFRESH_TOKEN
+                                </th>
+                                <td class="px-6 py-4">
+                                    <p class="overflow-hidden break-all whitespace-normal">
+                                        {{ Auth::user()->kc_refresh_token }}
+                                    </p>
+                                </td>
+                                <td class="px-6 py-4">
+                                    {{ Auth::user()->kc_refresh_token_expiration }}
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+
+            </div>
+        {{-- </div> --}}
+
     </div>
 </x-app-layout>
